63ef7d44af0241e2bae6d8e674268e1b48bd7862
Sheng <sheng.chen@giift.com>
2023-04-27 15:49:32 +0800

    Update

--- bin/deploy.ts
+++ bin/deploy.ts
@@ -15,7 +15,7 @@ new DeployStack(app, 'DeployStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '339737986233', region: 'ap-southeast-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/deploy-stack.ts
+++ lib/deploy-stack.ts
@@ -1,12 +1,17 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
 export class DeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
 
     // example resource
     // const queue = new sqs.Queue(this, 'DeployQueue', {
